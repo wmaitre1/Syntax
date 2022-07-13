4b4f9f4ba95250b3df8432b1576940ce38a65927
William Maitre <william.maitre@gmail.com>
2022-07-13 15:24:26 -0400

    finished exercise

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//int answer = 4;
+//string response;
+
+//if (answer < 9)
+//{
+//    response = answer + " is less than nine";
+//}
+//else
+//{
+ //   response = answer + “greater than or equal to nine”;
+//}
+
+var answer = 4;
+var response = (answer < 9) ? $"{answer} is less than nine!" : $"{answer} greater than or equal to nine";
